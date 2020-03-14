563aa5fc3ea724db70c588166d5d2b997cc34f39
Mattias Levlin <mattias.levlin@gmail.com>
2020-03-14 17:54:05 +0200

    Split tests into svg and div tests, with empty and nonempty variants

--- src/components/Benchmark.jsx
+++ src/components/Benchmark.jsx
@@ -6,38 +6,93 @@ class Benchmark extends Component {
         super(props);
 
         this.state = {
-            numberOfDivElements: 0,
+            numberOfElements: 0,
+            selectedTest: 0,
         }
     }
 
-    add1000DivElementsToTheDom = () => {
+    // Test 1, 2: Empty <div> elements
+
+    addEmptyDivs = () => {
         this.setState({
-            numberOfDivElements: 10000
+            numberOfElements: 10000,
+            selectedTest: 1,
         })
     }
 
-    delete1000DivElementsFromTheDom = () => {
+    removeEmptyDivs = () => {
         this.setState({
-            numberOfDivElements: 0
+            numberOfElements: 0,
+            selectedTest: 1,
+        })
+    }
+
+    // Test 3, 4: Nonempty <div> elements
+
+    addDivs = () => {
+        this.setState({
+            numberOfElements: 10000,
+            selectedTest: 2,
+        })   
+    }
+
+    removeDivs = () => {
+        this.setState({
+            numberOfElements: 0,
+            selectedTest: 2,
+        })
+    }
+
+    // Test 5, 6: empty <svg> elements
+
+    addEmptySvgs = () => {
+        this.setState({
+            numberOfElements: 10000,
+            selectedTest: 3,
+        })
+    }
+
+    removeEmptySvgs = () => {
+        this.setState({
+            numberOfElements: 0,
+            selectedTest: 3,
+        })
+    }
+
+    // Test 7, 8: nonempty <svg> elements
+
+    addSvgs = () => {
+        this.setState({
+            numberOfElements: 10000,
+            selectedTest: 4,
+        })
+    }
+
+    removeSvgs = () => {
+        this.setState({
+            numberOfElements: 0,
+            selectedTest: 4,
         })
     }
 
     render() {
         return (
             <div className="Benchmark">
-                <button
-                    onClick={this.add1000DivElementsToTheDom}
-                >
-                    Add 10,000 div elements
-                </button>
-                <button
-                    onClick={this.delete1000DivElementsFromTheDom}
-                >
-                    Remove 10,000 div elements
-                </button>
-                <BenchmarkContainer
-                    numberOfDivElements={this.state.numberOfDivElements}
-                />
+                <button onClick={this.addEmptyDivs}>1 addEmptyDivs</button>
+                <button onClick={this.removeEmptyDivs}>2 removeEmptyDivs</button>
+                <button onClick={this.addDivs}>3 addDivs</button>
+                <button onClick={this.removeDivs}>4 removeDivs</button>
+                <button onClick={this.addEmptySvgs}>5 addEmptySvgs</button>
+                <button onClick={this.removeEmptySvgs}>6 removeEmptySvgs</button>
+                <button onClick={this.addSvgs}>7 addSvgs</button>
+                <button onClick={this.removeSvgs}>8 removeSvgs</button>
+
+                <div>
+                    <BenchmarkContainer
+                        testSelection={this.state.selectedTest}
+                        numberOfElements={this.state.numberOfElements}
+                    />
+                </div>
             </div>
         );
     }
--- src/components/BenchmarkContainer.jsx
+++ src/components/BenchmarkContainer.jsx
@@ -3,15 +3,32 @@ import '../App.css';
 
 class BenchmarkContainer extends Component {
     render() {
-        const renderedOutput = [...Array(this.props.numberOfDivElements)].map(() =>
-            <div>
-                <p>DOM</p>
-            </div>
-        )
-
+        let renderedDomOutput = null
+        console.log(this.props.numberOfElements)
+        if (this.props.testSelection === 1) {
+            renderedDomOutput = [...Array(this.props.numberOfElements)].map(() =>
+                <div/>
+            )
+        } else if (this.props.testSelection === 2) {
+            renderedDomOutput = [...Array(this.props.numberOfElements)].map(() =>
+                <div>
+                    <p>div</p>
+                </div>
+            )
+        } else if (this.props.testSelection === 3) {
+            renderedDomOutput = [...Array(this.props.numberOfElements)].map(() =>
+                <svg/>
+            )
+        } else if (this.props.testSelection === 4) {
+            renderedDomOutput = [...Array(this.props.numberOfElements)].map(() =>
+                <svg>
+                    <text>svg</text>
+                </svg>
+            )
+        }
         return (
             <div>
-                {renderedOutput}
+                {renderedDomOutput}
             </div>
         );
     }
