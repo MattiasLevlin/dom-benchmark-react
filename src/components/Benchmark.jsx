6f98af1d820847c5aa91735ad9efe610636c60d4
Mattias Levlin <mattias.levlin@gmail.com>
2020-04-04 23:04:27 +0300

    Re-add BenchmarkContainer.jsx for comparison

--- src/components/Benchmark.jsx
+++ src/components/Benchmark.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
+import BenchmarkContainer from './BenchmarkContainer.jsx';
+
 
 let operationStartTime = null
 let operationEndTime = null
@@ -10,6 +12,8 @@ class Benchmark extends Component {
         this.state = {
             items: [],
             selectedTest: 0,
+            ingredients: [],
+            
         };
         // this.addDiv = this.addDiv.bind(this);
     }
@@ -17,6 +21,13 @@ class Benchmark extends Component {
     componentDidUpdate() {
         operationEndTime = performance.now()
         console.log('Operation took ' + (operationEndTime - operationStartTime) + ' milliseconds')
+    }
+
+    addDivMap() {
+        this.setState({
+            ingredients: 1000
+        })
+        this.forceUpdate()
     }
 
     // 1 
@@ -59,15 +70,47 @@ class Benchmark extends Component {
         console.log('TEST 5) REMOVE 10000 DIVS')
     }
 
+
+    // 1  BENCHMARK CONTAINER add all
+    addDivsBenchmarkContainer = () => {
+        this.setState({
+            selectedTest: 1,
+        })
+        operationStartTime = performance.now()
+    }
+
+    // 5 BENCHMARK CONTAINER remove all
+    removeDivBenchmarkCOntainer = () => {
+        this.setState({
+            selectedTest: 3,
+        })
+        operationStartTime = performance.now()
+    }
+
     render() {
         return (
             <div className="Benchmark">
-                <p>div</p>
+                <p>In component</p>
+                <button onClick={() => this.addDivMap()}>0 MAP</button>
                 <button onClick={() => this.addDiv()}>1 addDiv</button>
                 <button onClick={() => this.editOneDiv()}>2 editDiv</button>
                 <button onClick={() => this.removeAllDiv()}>5 removeAllDiv</button>
+
+                <p>BenchmarkContainer</p>
+                <button onClick={this.addDivsBenchmarkContainer}>1 add BC</button>
+                <button onClick={this.removeDivBenchmarkCOntainer}>5 remove BC</button>
+
                 <div>
                     {this.state.items}
+                </div>
+                <div>{this.state.ingredients.map((abc, i) =>
+                    <p key={abc}>Div {abc}</p>
+                )}
+                </div>
+                <div>
+                    <BenchmarkContainer
+                        testSelection={this.state.selectedTest}
+                    />
                 </div>
             </div>
         );
